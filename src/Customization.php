00e26b986c4df6730ee2887ad2ce0698f5169f5c
Jiri Spacek <jiri@jirispacek.info>
2019-11-18 14:12:08 +0100

    fix

--- src/Customization.php
+++ src/Customization.php
@@ -12,7 +12,6 @@
 namespace Grido;
 
 use Grido\Grid;
-use Nette\Object;
 use Nette\SmartObject;
 
 /**
@@ -29,112 +28,112 @@ class Customization
 
 	use SmartObject;
 
-    const TEMPLATE_DEFAULT = 'default';
-    const TEMPLATE_BOOTSTRAP = 'bootstrap';
+	const TEMPLATE_DEFAULT = 'default';
+	const TEMPLATE_BOOTSTRAP = 'bootstrap';
 
-    /** @var Grid */
-    protected $grid;
+	/** @var Grid */
+	protected $grid;
 
-    /** @var string|array */
-    protected $buttonClass;
+	/** @var string|array */
+	protected $buttonClass;
 
-    /** @var string|array */
-    protected $iconClass;
+	/** @var string|array */
+	protected $iconClass;
 
-    /** @var array */
-    protected $templateFiles = [];
+	/** @var array */
+	protected $templateFiles = [];
 
-    /**
-     * @param Grid $grid
-     */
-    public function __construct(Grid $grid)
-    {
-        $this->grid = $grid;
-    }
+	/**
+	 * @param Grid $grid
+	 */
+	public function __construct(Grid $grid)
+	{
+		$this->grid = $grid;
+	}
 
-    /**
-     * @param string|array $class
-     * @return \Grido\Customization
-     */
-    public function setButtonClass($class)
-    {
-        $this->buttonClass = $class;
-        return $this;
-    }
+	/**
+	 * @param string|array $class
+	 * @return \Grido\Customization
+	 */
+	public function setButtonClass($class)
+	{
+		$this->buttonClass = $class;
+		return $this;
+	}
 
-    /**
-     * @param string|array $class
-     * @return \Grido\Customization
-     */
-    public function setIconClass($class)
-    {
-        $this->iconClass = $class;
-        return $this;
-    }
+	/**
+	 * @param string|array $class
+	 * @return \Grido\Customization
+	 */
+	public function setIconClass($class)
+	{
+		$this->iconClass = $class;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getButtonClass()
-    {
-        return is_array($this->buttonClass)
-            ? implode(' ', $this->buttonClass)
-            : $this->buttonClass;
-    }
+	/**
+	 * @return string
+	 */
+	public function getButtonClass()
+	{
+		return is_array($this->buttonClass)
+			? implode(' ', $this->buttonClass)
+			: $this->buttonClass;
+	}
 
-    /**
-     * @param string $icon
-     * @return string
-     */
-    public function getIconClass($icon = NULL)
-    {
-        if ($icon === NULL) {
-            $class = $this->iconClass;
-        } else {
-            $this->iconClass = (array) $this->iconClass;
-            $classes = [];
-            foreach ($this->iconClass as $fontClass) {
-                $classes[] = "{$fontClass} {$fontClass}-{$icon}";
-            }
-            $class = implode(' ', $classes);
-        }
+	/**
+	 * @param string $icon
+	 * @return string
+	 */
+	public function getIconClass($icon = NULL)
+	{
+		if ($icon === NULL) {
+			$class = $this->iconClass;
+		} else {
+			$this->iconClass = (array)$this->iconClass;
+			$classes = [];
+			foreach ($this->iconClass as $fontClass) {
+				$classes[] = "{$fontClass} {$fontClass}-{$icon}";
+			}
+			$class = implode(' ', $classes);
+		}
 
-        return $class;
-    }
+		return $class;
+	}
 
-    /**
-     * @return array
-     */
-    public function getTemplateFiles()
-    {
-        if (empty($this->templateFiles)) {
-            foreach (new \DirectoryIterator(__DIR__ . '/templates') as $file) {
-                if ($file->isFile()) {
-                    $this->templateFiles[$file->getBasename('.latte')] = realpath($file->getPathname());
-                }
-            }
-        }
+	/**
+	 * @return array
+	 */
+	public function getTemplateFiles()
+	{
+		if (empty($this->templateFiles)) {
+			foreach (new \DirectoryIterator(__DIR__ . '/templates') as $file) {
+				if ($file->isFile()) {
+					$this->templateFiles[$file->getBasename('.latte')] = realpath($file->getPathname());
+				}
+			}
+		}
 
-        return $this->templateFiles;
-    }
+		return $this->templateFiles;
+	}
 
-    /**
-     * Default theme.
-     * @return \Grido\Customization
-     */
-    public function useTemplateDefault()
-    {
-        $this->grid->setTemplateFile($this->getTemplateFiles()[self::TEMPLATE_DEFAULT]);
-        return $this;
-    }
+	/**
+	 * Default theme.
+	 * @return \Grido\Customization
+	 */
+	public function useTemplateDefault()
+	{
+		$this->grid->setTemplateFile($this->getTemplateFiles()[self::TEMPLATE_DEFAULT]);
+		return $this;
+	}
 
-    /**
-     * Twitter Bootstrap theme.
-     * @return \Grido\Customization
-     */
-    public function useTemplateBootstrap()
-    {
-        $this->grid->setTemplateFile($this->getTemplateFiles()[self::TEMPLATE_BOOTSTRAP]);
-        return $this;
-    }
+	/**
+	 * Twitter Bootstrap theme.
+	 * @return \Grido\Customization
+	 */
+	public function useTemplateBootstrap()
+	{
+		$this->grid->setTemplateFile($this->getTemplateFiles()[self::TEMPLATE_BOOTSTRAP]);
+		return $this;
+	}
 }
